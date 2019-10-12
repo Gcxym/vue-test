d29b474f89bf7fe06071cd79a37c2969f58ffeec
曹勇 <ycao@dunoinfo.com>
2019-10-12 17:35:13 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "@babel/polyfill"; //兼容低版本内核游览器打不开情况
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI, {
+    size: 'small',
+    zIndex: 3000
+});
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
---
+++ src/view/defanult/login/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>2333</div>
+</template>
+<script>
+export default {
+  name: "login"
+};
+</script>
+<style lang="scss">
+</style>
