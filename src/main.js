cf63e24ac88ec6cbb083bf90d4d6f14868651ada
cxycom <cxymm22@gmail.com>
2019-10-13 10:08:33 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import "@babel/polyfill"; //兼容低版本内核游览器打不开情况
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI, {
-    size: 'small',
-    zIndex: 3000
-});
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-    render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
