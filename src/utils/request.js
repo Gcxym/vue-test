01f3368aaf4d71d7d058934748b7357ff4ebde55
cxycom <cxymm22@gmail.com>
2019-10-17 07:58:40 +0800

    init

---
+++ src/utils/request.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+class HttpRequest {
+    constructor(baseUrl = baseURL) { //不懂
+        this.baseUrl = baseUrl
+        this.queue = {}
+    }
+    getInsideConfig() {
+        const config = {
+            baseURL: this.baseUrl,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            credentials: 'same-origin',
+            withCredentials: true   //表示跨域请求时是否需要使用凭证
+        }
+    }
+}
