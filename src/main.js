3ce21edea07b2dc4bc44a002687bbbb9aa24e11a
cxycom <cxymm22@gmail.com>
2019-10-30 22:15:00 +0800

    路由配置

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
+// 实际打包时应该不引入mock
+if (process.env.NODE_ENV !== 'production') require('@/mock')
+
 new Vue({
   router,
   store,
